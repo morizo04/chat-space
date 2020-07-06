c87e4c55bed00a8bf7c67d8dc0eccf7e2ad27f9d
morizo04 <omorikazuki99@gmail.com>
2020-07-06 18:55:39 +0900

    機能のブラッシュアップ
    動作チェック問題なし

--- app/assets/javascripts/modules/message.js
+++ app/assets/javascripts/modules/message.js
@@ -1,36 +1,4 @@
 $(function(){
-
-  let reloadMessages = function() {
-    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
-    let last_message_id = $('.main-contents__chat-space__text:last').data("message-id");
-    $.ajax({
-      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
-      url: "api/messages",
-      //ルーティングで設定した通りhttpメソッドをgetに指定
-      type: 'get',
-      dataType: 'json',
-      //dataオプションでリクエストに値を含める
-      data: {id: last_message_id}
-    })
-    .done(function(messages) {
-      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
-      if (messages.length !== 0) {
-        //追加するHTMLの入れ物を作る
-        let insertHTML = '';
-        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
-        $.each(messages, function(i, message) {
-          insertHTML += buildHTML(message)
-        });
-        //メッセージが入ったHTMLに、入れ物ごと追加
-        $('.MessageField').append(insertHTML);
-      }
-    })
-    .fail(function() {
-      alert('error');
-    });
-  };
-
-
   function buildHTML(message){
     if ( message.image ) {
       let html =
@@ -72,7 +40,6 @@ $(function(){
     };
   }
 
-
   $(".Form").on('submit', function(e){
     e.preventDefault()
     let formData = new FormData(this);
@@ -94,9 +61,10 @@ $(function(){
     })
     .fail(function() {
       alert("メッセージ送信に失敗しました");
+      $('.main-contents__text-form__submit').prop("disabled", false);
     });
   });
 
 
-  setInterval(reloadMessages, 7000);
+  
 });
