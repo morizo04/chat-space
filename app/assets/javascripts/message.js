81b6f7bdbc62dff17b82843144c2c920f5e29048
morizo04 <omorikazuki99@gmail.com>
2020-07-06 18:11:34 +0900

    取得して投稿データの表示
    カリキュラム通りに実行

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,8 +1,40 @@
 $(function(){
+
+  let reloadMessages = function() {
+    //カスタムデータ属性を利用し、ブラウザに表示されている最新メッセージのidを取得
+    let last_message_id = $('.main-contents__chat-space__text:last').data("message-id");
+    $.ajax({
+      //ルーティングで設定した通り/groups/id番号/api/messagesとなるよう文字列を書く
+      url: "api/messages",
+      //ルーティングで設定した通りhttpメソッドをgetに指定
+      type: 'get',
+      dataType: 'json',
+      //dataオプションでリクエストに値を含める
+      data: {id: last_message_id}
+    })
+    .done(function(messages) {
+      // 更新するメッセージがなかった場合は.doneの後の処理が動かないようにする
+      if (messages.length !== 0) {
+        //追加するHTMLの入れ物を作る
+        let insertHTML = '';
+        //配列messagesの中身一つ一つを取り出し、HTMLに変換したものを入れ物に足し合わせる
+        $.each(messages, function(i, message) {
+          insertHTML += buildHTML(message)
+        });
+        //メッセージが入ったHTMLに、入れ物ごと追加
+        $('.MessageField').append(insertHTML);
+      }
+    })
+    .fail(function() {
+      alert('error');
+    });
+  };
+
+
   function buildHTML(message){
     if ( message.image ) {
       let html =
-        `<div class="main-contents__chat-space__text">
+        `<div class="main-contents__chat-space__text" data-message-id=${message.id}>
           <div class="main-contents__chat-space__text__user">
             <div class="main-contents__chat-space__text__user__name">
               ${message.user_name}
@@ -21,7 +53,7 @@ $(function(){
       return html;
     } else {
       let html =
-      `<div class="main-contents__chat-space__text">
+      `<div class="main-contents__chat-space__text" data-message-id=${message.id}>
         <div class="main-contents__chat-space__text__user">
           <div class="main-contents__chat-space__text__user__name">
             ${message.user_name}
@@ -64,4 +96,7 @@ $(function(){
       alert("メッセージ送信に失敗しました");
     });
   });
+
+
+  setInterval(reloadMessages, 7000);
 });
